30f7286dcf6608c21cb7ac73e8b7a470c78970cf
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-07 17:42:07 -0400

    Added component for the teams

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import "./App.css";
+import Team from "../src/components/team.js";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [tiger, setTiger] = useState(0);
   const [lion, setLion] = useState(0);
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{lion}</div>
-          </div>
+          <Team name = "Yeets" isHome = {true} score = {lion}/>
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{tiger}</div>
-          </div>
+          <Team name = "Yates" isHome = {false} score = {tiger}/> 
         </div>
         <BottomRow />
       </section>
---
+++ src/components/team.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Team(props){
+    const teamName = props.name;
+    const TeamLocal = props.isHome ? 'home' : 'away';
+
+    let score = props.score;
+
+    return(
+        <div className = {TeamLocal}>
+            <h2 className = {`${TeamLocal}__name`}>{teamName}</h2>
+            <div className = {`${TeamLocal}__score`}>{score}</div>
+        </div>
+    )
+}
+
+export default Team;
